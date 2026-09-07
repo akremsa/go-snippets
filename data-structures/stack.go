b1ee6340f18acc2eaabb023c43be5f9bf40bee62
akremsa <10kdmg@gmail.com>
2026-09-07 22:12:14 +0300

    add stack implementation

---
+++ data-structures/stack.go
@@ -0,0 +1,51 @@
+package datastructures
+
+import "slices"
+
+type Stack[T any] struct {
+	items []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		items: make([]T, 0),
+	}
+}
+
+func (s *Stack[T]) Push(item T) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	var zero T
+	if len(s.items) == 0 {
+		return zero, false
+	}
+
+	last := len(s.items) - 1
+	res := s.items[last]
+
+	s.items[last] = zero
+	s.items = s.items[:last]
+	return res, true
+}
+
+func (s *Stack[T]) Peek() (T, bool) {
+	if len(s.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	return s.items[len(s.items)-1], true
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.items)
+}
+
+func (s *Stack[T]) Reverse() {
+	slices.Reverse(s.items)
+}
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	gopatterns "go-snippets/go-patterns"
+	"fmt"
+	datastructures "go-snippets/data-structures"
 )
 
 func main() {
-	gopatterns.ErrorsIsAs()
+	// TESTING INT STACK
+	intStack := datastructures.NewStack[int]()
+	intStack.Push(10)
+	intStack.Push(20)
+	intStack.Push(30)
+
+	fmt.Println("Int Stack Size:", intStack.Size()) // Should print 3
+
+	top, ok := intStack.Pop()
+	fmt.Printf("Popped: %d, OK: %t\n", top, ok) // Should print 30, true
+
+	peek, ok := intStack.Peek()
+	fmt.Printf("Peek: %d, OK: %t\n", peek, ok) // Should print 20, true
+
+	intStack.Reverse()
+	peek, ok = intStack.Peek()
+	fmt.Println("After Reverse, Top:", peek) // Should print 10 (since stack was [10,20], reversed -> [20,10], top is 10)
+
+	fmt.Println("Is empty? ", intStack.IsEmpty())
 }
